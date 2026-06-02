0b74a7113946eaf64abcba6d95c8942656202563
omarothman05 <omartrad200536@gmail.com>
2026-06-02 21:07:25 +0300

    TaskFlow Full Stack Project

---
+++ README.md
@@ -0,0 +1,2 @@
+# TaskFlow
+A full-stack task management application that allows users to register, log in, and manage their personal tasks with a clean and responsive interface.
---
+++ server/server.js
@@ -0,0 +1,183 @@
+require("dotenv").config();
+
+const express = require("express");
+const connectDB = require("./db");
+
+const User = require("./models/User");
+const Task = require("./models/Task");
+
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  try {
+    const token = authHeader.replace("Bearer ", "");
+
+    const decoded = jwt.verify(token, "SECRET_KEY");
+
+    req.userId = decoded.id;
+
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("TaskFlow API is running");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const user = new User({
+      email: req.body.email,
+      password: hashedPassword
+    });
+
+    await user.save();
+
+    const token = jwt.sign(
+  {
+    id: user._id,
+    email: user.email,
+  },
+  "SECRET_KEY",
+  {
+    expiresIn: "1d",
+  }
+);
+
+res.json({
+  message: "Register successful",
+  token,
+});
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Wrong password" });
+    }
+
+    const token = jwt.sign(
+  {
+    id: user._id,
+    email: user.email,
+  },
+  "SECRET_KEY",
+  {
+    expiresIn: "1d",
+  }
+);
+
+    res.json({
+      message: "Login successful",
+      token
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post("/tasks", verifyToken, async (req, res) => {
+  try {
+    const task = new Task({
+      title: req.body.title,
+      userId: req.userId
+    });
+
+    await task.save();
+
+    res.json(task);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+app.get("/tasks", verifyToken, async (req, res) => {
+  try {
+    const tasks = await Task.find({ userId: req.userId });
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.put("/tasks/:id", verifyToken, async (req, res) => {
+  try {
+    const task = await Task.findOneAndUpdate(
+      {
+        _id: req.params.id,
+        userId: req.userId
+      },
+      {
+        title: req.body.title,
+        completed: req.body.completed
+      },
+      { new: true }
+    );
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.json(task);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.delete("/tasks/:id", verifyToken, async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      userId: req.userId
+    });
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.json({ message: "Task deleted" });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
